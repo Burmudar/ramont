b6ce3102ee3dacdf3850106b14c5612de32b4a04
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2022-03-21 15:57:44 +0200

    add Trackpad virtual device

--- ramont-rs/src/ramont/device.rs
+++ ramont-rs/src/ramont/device.rs
@@ -1,14 +1,27 @@
 use std::sync::Mutex;
 
 use evdev_rs::{
-    enums::{BusType, EventCode, EventType, EV_KEY, EV_REL, EV_SYN},
+    enums::{BusType, EventCode, EventType, InputProp, EV_ABS, EV_KEY, EV_REL, EV_SYN},
     DeviceWrapper, InputEvent, TimeVal, UInputDevice, UninitDevice,
 };
 
 pub struct Mouse {
     name: String,
     device: UInputDevice,
-    last_move: Mutex<Move>,
+    last_move: Mutex<Coordinate>,
+}
+
+pub struct Trackpad {
+    name: String,
+    device: UInputDevice,
+    last_move: Mutex<Coordinate>,
+    x_dimension: Dimension,
+    y_dimension: Dimension,
+}
+
+pub trait VirtualDevice {
+    fn r#move(&self, x: i32, y: i32) -> Result<(), std::io::Error>;
+    fn sync(&self) -> Result<(), std::io::Error>;
 }
 
 struct Coordinate {
@@ -16,14 +29,9 @@ struct Coordinate {
     y: i32,
 }
 
-enum Move {
-    Point(Coordinate),
-}
-
-impl Move {
-    fn new(x: i32, y: i32) -> Move {
-        Move::Point(Coordinate { x, y })
-    }
+struct Dimension {
+    width: i32,
+    height: i32,
 }
 
 const ZERO_TIME: TimeVal = TimeVal::new(0, 0);
@@ -68,14 +76,15 @@ impl Mouse {
         Mouse {
             name: name.to_string(),
             device: udevice,
-            last_move: Mutex::new(Move::new(0, 0)),
+            last_move: Mutex::new(Coordinate { x: 0, y: 0 }),
         }
     }
+}
 
-    pub fn r#move(&self, x: i32, y: i32) -> Result<(), std::io::Error> {
-        let p = *self.last_move.lock().unwrap();
+impl VirtualDevice for Mouse {
+    fn r#move(&self, x: i32, y: i32) -> Result<(), std::io::Error> {
+        let c = *self.last_move.lock().unwrap();
 
-        let Move::Point(c) = p;
         // we do this to get the delta of the the old and new point
         // essentially we're answering "How many units must I move from the old position to the new
         // position.
@@ -99,7 +108,106 @@ impl Mouse {
         self.sync()
     }
 
-    pub fn sync(&self) -> Result<(), std::io::Error> {
+    fn sync(&self) -> Result<(), std::io::Error> {
+        self.device.write_event(&InputEvent::new(
+            &ZERO_TIME,
+            &EventCode::EV_SYN(EV_SYN::SYN_REPORT),
+            0,
+        ))
+    }
+}
+
+impl Trackpad {
+    pub fn new(name: &str, x_dimension: Dimension, y_dimension: Dimension) -> Trackpad {
+        let device = UninitDevice::new().unwrap();
+
+        device.set_name(name);
+
+        let x_abs_info = evdev_rs::AbsInfo {
+            value: 0,
+            minimum: x_dimension.width,
+            maximum: x_dimension.height,
+            fuzz: 0,
+            flat: 0,
+            resolution: 1080,
+        };
+        let y_abs_info = evdev_rs::AbsInfo {
+            value: 0,
+            minimum: y_dimension.width,
+            maximum: y_dimension.height,
+            fuzz: 0,
+            flat: 0,
+            resolution: 1080,
+        };
+        device.set_abs_info(&EventCode::EV_ABS(EV_ABS::ABS_X), &x_abs_info);
+        device.set_abs_info(&EventCode::EV_ABS(EV_ABS::ABS_Y), &y_abs_info);
+
+        device
+            .enable(&EventType::EV_ABS)
+            .expect("failed to enable event type EV_ABS");
+        device
+            .enable(&EventCode::EV_ABS(EV_ABS::ABS_X))
+            .expect("failed to enable event ABS_X");
+        device
+            .enable(&EventCode::EV_ABS(EV_ABS::ABS_Y))
+            .expect("failed to enable event ABS_Y");
+
+        device.enable(&EventType::EV_KEY);
+        device
+            .enable(&EventCode::EV_KEY(EV_KEY::KEY_TOUCHPAD_ON))
+            .expect("failed to enable KEY_TOUCHPAD_ON");
+        device
+            .enable(&EventCode::EV_KEY(EV_KEY::KEY_TOUCHPAD_OFF))
+            .expect("failed to enable KEY_TOUCHPAD_OFF");
+        device
+            .enable(&InputProp::INPUT_PROP_DIRECT)
+            .expect("failed to enable property INPUT_PROP_DIRECT");
+
+        device
+            .enable(&EventCode::EV_KEY(EV_KEY::BTN_LEFT))
+            .expect("failed to enable EV_KEY BTN_LEFT");
+        device
+            .enable(&EventCode::EV_KEY(EV_KEY::BTN_MIDDLE))
+            .expect("failed to enable EV_KEY BTN_MIDDLE");
+        device
+            .enable(&EventCode::EV_KEY(EV_KEY::BTN_RIGHT))
+            .expect("failed to enable EV_KEY BTN_RIGHT");
+
+        let udevice = UInputDevice::create_from_device(&device)
+            .expect("failed to create UInputDevice from raw device");
+
+        Trackpad {
+            name: name.to_string(),
+            device: udevice,
+            x_dimension,
+            y_dimension,
+            last_move: Mutex::new(Coordinate { x: 0, y: 0 }),
+        }
+    }
+}
+
+impl VirtualDevice for Trackpad {
+    fn r#move(&self, x: i32, y: i32) -> Result<(), std::io::Error> {
+        let c = *self.last_move.lock().unwrap();
+
+        self.device.write_event(&InputEvent::new(
+            &ZERO_TIME,
+            &EventCode::EV_ABS(EV_ABS::ABS_Y),
+            x,
+        ));
+        self.device.write_event(&InputEvent::new(
+            &ZERO_TIME,
+            &EventCode::EV_ABS(EV_ABS::ABS_X),
+            y,
+        ));
+
+        c.x = x;
+        c.y = y;
+
+        self.sync()
+    }
+
+    fn sync(&self) -> Result<(), std::io::Error> {
         self.device.write_event(&InputEvent::new(
             &ZERO_TIME,
             &EventCode::EV_SYN(EV_SYN::SYN_REPORT),
